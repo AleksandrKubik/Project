b0beb24adce81562f45f6750b7868cf9046512ef
AleksandrKubik <109134492+AleksandrKubik@users.noreply.github.com>
2022-07-20 08:10:28 +0300

    Empty Project
    1 commit. Add first files.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src = 'https://avatars.mds.yandex.net/i?id=043a392fe02d6b0d1c9ce9a8d9864fdf-5286894-images-thumbs&n=13' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div className='banner'><img src='https://avatars.mds.yandex.net/i?id=ea922f54d8727c82c5365d3375cdfd7a-5221954-images-thumbs&n=13' /></div>
+    <div className='avatar'>
+      <img src='https://avatars.mds.yandex.net/i?id=75a90ce2d4a1251737d66f55c134ae26-4461245-images-thumbs&n=13'></img>
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        post1
+      </div>
+      <div>
+        post2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
